22714d6004a9ba63d5c5d84499c8b21d41e10584
Nikolay Koren <nikolai.koren2004@gmail.com>
2023-06-26 10:21:51 +0000

    UnitTest5.0-corrections

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -8,20 +8,21 @@ public class SquareEquation
 
         if (Math.Abs(a) < eps) throw new System.ArgumentException();
         foreach(double x in new double[] {a, b, c}){
-            if (double.IsNaN(x) || double.IsInfinity(x)) throw new System.ArgumentException();
+            if (double.IsNaN(x) | double.IsInfinity(x)) throw new System.ArgumentException();
         }
 
         double D = b*b - 4*a*c;
 
+        double x1;
+        if(b == 0) x1 = -(b + Math.Sqrt(D))/2;
+        else x1 = -(b + Math.Sign(b)*Math.Sqrt(D))/2;
+        double x2 = c/x1;
+
         if (Math.Abs(D) < eps){
-            double x = -(b + Math.Sign(b)*Math.Sqrt(D))/2;
-            return new double[] {x};
+            return new double[] {x1};
         }
 
         if(D < 0) return new double[0];
-
-        double x1 = -(b + Math.Sign(b)*Math.Sqrt(D))/2;
-        double x2 = c/x1;
 
         return new double[] {x1, x2};
     }
