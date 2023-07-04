81c5424a162dae7b906f0ae0895b7068ced90338
Nikolay Koren <nikolai.koren2004@gmail.com>
2023-07-04 16:30:59 +0000

    init

---
+++ Pool/Pool.cs
@@ -0,0 +1,30 @@
+﻿namespace Pool;
+
+public class SpaceShip
+{
+
+}
+
+public class Pool<T>
+{
+	public PoolSlot<T> TakeSlot() {} // операция "взять из пула"
+	public void Release(PoolSlot<T> slot) {} // операция "вернуть в пул"
+	
+	/* ... */
+	
+	// методы для переопределения:
+	protected abstract T ObjectConstructor(); // создание нового объекта, готового для использования
+	protected virtual void CleanUp(T item) {} // очистка использованного объекта, вызываемая автоматически
+}
+
+public class PoolGuard
+{
+
+}
+
+class Program
+{
+    static void Main(string[] args){
+
+    }
+}
