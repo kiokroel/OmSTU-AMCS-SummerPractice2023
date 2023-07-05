11da2c8f0f07df245f6d8c4e627bd03f96ef7863
Nikolay Koren <nikolai.koren2004@gmail.com>
2023-07-05 08:56:45 +0000

    fin

---
+++ Pool/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Pool_10;
+
+class Program
+{
+    static void Main(string[] args){
+		int n = 7;
+		Pool<SpaceShip> spaceShipPool = new Pool<SpaceShip>(n);
+		using(PoolGuard<SpaceShip> poolGuard = new PoolGuard<SpaceShip>(spaceShipPool)){
+			SpaceShip spaceShip = poolGuard.TakeObj;
+		}
+    }
+}
